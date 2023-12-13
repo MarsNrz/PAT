ad32c650878123f6b8df428ef0588bbc137dd3fe
gavrilahana <gav.hana1@gmail.com>
2023-12-14 00:27:25 +0700

    update exception dan log

--- app/Http/Controllers/DataAlatController.php
+++ app/Http/Controllers/DataAlatController.php
@@ -5,22 +5,60 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\dataAlat;
 use Illuminate\Http\Request;
+use DataTables;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class DataAlatController extends Controller
 {
     public function index()
     {
-        $data = dataAlat::all();
-        return view ('dataAlat', compact('data'));
+        try {
+            $data = dataAlat::all();
+            return view('dataAlat', compact('data'));
+        } catch (Exception $e) {
+            // Tangani exception di sini, misalnya log atau kirim notifikasi
+            Log::error('Error in DataAlatController@index: ' . $e->getMessage());
+            return response()->view('errors.500', [], 500); // Tampilkan halaman error 500
+        }
     }
+
     public function tambahDataAlat()
     {
-        return view ('tambahDataAlat');
+        return view('tambahDataAlat');
     }
+
     public function insertDataAlat(Request $request)
     {
-        dataAlat::create($request->all());
-        return redirect()->route('Alat')->with ('success', 'Data berhasil di Tambahkan');
+        try {
+            dataAlat::create($request->all());
+            return redirect()->route('Alat')->with('success', 'Data berhasil di Tambahkan');
+        } catch (Exception $e) {
+            // Tangani exception di sini, misalnya log atau kirim notifikasi
+            Log::error('Error in DataAlatController@insertDataAlat: ' . $e->getMessage());
+            return redirect()->route('Alat')->with('error', 'Terjadi kesalahan: ' . $e->getMessage());
+        }
+    }
+
+    // ... Fungsi lainnya ...
+
+    public function akunJson()
+    {
+        try {
+            $data = dataAlat::all();
+            return dataAlat::of($data)
+                ->addColumn('action', function ($item) {
+                    $btnEdit = '<a href="/editAlat/' . $item->id_akun . '" class="btn btn-primary">Edit</a>';
+                    $btnDelete = '<a href="/deleteAlat/' . $item->id_akun . '" class="btn btn-danger">Delete</a>';
+                    return $btnEdit . $btnDelete;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        } catch (Exception $e) {
+            // Tangani exception di sini, misalnya log atau kirim notifikasi
+            Log::error('Error in DataAlatController@akunJson: ' . $e->getMessage());
+            return response()->json(['error' => 'Terjadi kesalahan: ' . $e->getMessage()], 500);
+        }
     }
 
     public function editDataAlat($id_alat)
@@ -42,19 +80,6 @@ class DataAlatController extends Controller
         $data = dataAlat::find($id_alat);
         $data->delete();
         return redirect()->route('Alat')->with('success', 'Data berhasil dihapus');
-    }
-   
-    public function akunJson()
-    {
-        $user = dataAlat::all();
-        return DataTables::of($data)
-            ->addColumn('action', function ($item) {
-                $btnEdit = '<a href="/editAlat/'.$item->id_akun.'" class="btn btn-primary">Edit</a>';
-                $btnDelete = '<a href="/deleteAlat/'.$item->id_akun.'" class="btn btn-danger">Delete</a>';
-                return $btnEdit.$btnDelete;
-            })
-            ->rawColumns(['action'])
-            ->make(true);
     }
 
     public function home()
